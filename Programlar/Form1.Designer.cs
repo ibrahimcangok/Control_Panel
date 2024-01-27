ca9163109c6cd73b77e6c137ee3414556c503c33
İbrahim Can Gök <ibrrahimgok00@gmail.com>
2024-01-27 19:08:54 +0300

    Proje dosyası ekle.

---
+++ Programlar/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Programlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BütünProgramlar = new System.Windows.Forms.ListBox();
+            this.MicrosoftProgramları = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // BütünProgramlar
+            // 
+            this.BütünProgramlar.FormattingEnabled = true;
+            this.BütünProgramlar.Location = new System.Drawing.Point(12, 12);
+            this.BütünProgramlar.Name = "BütünProgramlar";
+            this.BütünProgramlar.Size = new System.Drawing.Size(347, 316);
+            this.BütünProgramlar.TabIndex = 2;
+            // 
+            // MicrosoftProgramları
+            // 
+            this.MicrosoftProgramları.FormattingEnabled = true;
+            this.MicrosoftProgramları.Location = new System.Drawing.Point(363, 12);
+            this.MicrosoftProgramları.Name = "MicrosoftProgramları";
+            this.MicrosoftProgramları.Size = new System.Drawing.Size(347, 316);
+            this.MicrosoftProgramları.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Saat";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(268, 334);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(722, 388);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MicrosoftProgramları);
+            this.Controls.Add(this.BütünProgramlar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox BütünProgramlar;
+        private System.Windows.Forms.ListBox MicrosoftProgramları;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Programlar/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Data.OleDb;
+using Microsoft.VisualBasic;
+
+namespace Programlar
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con;
+        string pcadi = Environment.MachineName;
+        public Form1()
+        {
+            con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=vt1.mdb");
+            InitializeComponent();
+            tablo_olusturma();
+        }
+
+        void tablo_olusturma()
+        {
+            try
+            {
+                OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=vt1.mdb");
+                baglanti.Open();
+                OleDbCommand komut = new OleDbCommand("Create Table " + Environment.MachineName + " ([ID] counter primary key,[program] varchar(255),[tarih] Date/Time,[saat] Date/Time)", baglanti);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Bilgisayar Kaydı Başarıyla Oluşturuldu !!!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Tablonuz Oluşturuldu !!!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
+            try
+            {
+                BütünProgramlar.Items.Clear();
+                MicrosoftProgramları.Items.Clear();
+                string prganahtar = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+                RegistryKey anahtar = Registry.LocalMachine.OpenSubKey(prganahtar);
+                string[] altanahtar = anahtar.GetSubKeyNames();
+                string[] programlar = new string[altanahtar.Length];
+                int sonuc = -1;
+                for (int i = 0; i < altanahtar.Length; i++)
+                {
+                    RegistryKey SubKey = anahtar.OpenSubKey(altanahtar[i]);
+                    try
+                    {
+                        object displayNameObj = SubKey.GetValue("DisplayName");
+                        if (displayNameObj != null)
+                        {
+                            string prgismi = displayNameObj.ToString().Trim();
+                            if (!String.IsNullOrEmpty(prgismi))
+                            {
+                                programlar[i] = prgismi;
+                                sonuc = programlar[i].IndexOf("Microsoft");
+                                if (sonuc >= 0)
+                                {
+                                    MicrosoftProgramları.Items.Add(programlar[i]);
+                                }
+                                BütünProgramlar.Items.Add(programlar[i]);
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Application.DoEvents();
+                    }
+                    finally
+                    {
+                        SubKey?.Close();
+                    }
+                }
+
+            
+                anahtar.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hata !!!");
+            }
+            OleDbConnection bag = new OleDbConnection();
+            OleDbCommand sorgu = new OleDbCommand();
+            bag.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=vt1.mdb");
+            bag.Open();
+            sorgu.Connection = bag;
+            for (int i = 0; i < MicrosoftProgramları.Items.Count; i++)
+            {
+                sorgu.CommandText = "INSERT INTO MONSTER (program,tarih,saat)" + "VALUES('" + MicrosoftProgramları.Items[i].ToString() + "','" + Convert.ToDateTime(dateTimePicker1.Text) + "','" + label1.Text + "')";
+                sorgu.ExecuteNonQuery();
+            }
+
+            bag.Close();
+            MessageBox.Show("Kayıt İşlemi Tamamlandı");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
